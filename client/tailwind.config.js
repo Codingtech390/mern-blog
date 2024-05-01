d34cd0dc18395630c2daa28171e6c3711915addb
codingtech <codingtech390@gmail.com>
2024-05-01 13:54:11 +0530

    created the header section

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,15 @@
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     './pages/**/*.{html,js}',
+    flowbite.content(),
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [flowbite.plugin()],
 }
