d65475ed531b08744359d96d7e78a72e838c1d93
codingtech <codingtech390@gmail.com>
2024-05-08 12:26:20 +0530

    Added admin functionality to the user.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -54,7 +54,10 @@ export const signin = async (req, res, next) => {
       return next(errorHandler(400, "Invalid password!"));
     }
     // Generate a token
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET_KEY);
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET_KEY
+    );
 
     // Remove the password
     const { password: pass, ...rest } = validUser._doc;
@@ -78,7 +81,10 @@ export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+      const token = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.JWT_SECRET_KEY
+      );
       const { password, ...rest } = user._doc;
 
       res
@@ -106,7 +112,10 @@ export const google = async (req, res, next) => {
       });
       // Save the newly created user and create a new token for it.
       await newUser.save();
-      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET_KEY);
+      const token = jwt.sign(
+        { id: newUser._id, isAdmin: newUser.isAdmin },
+        process.env.JWT_SECRET_KEY
+      );
       const { password, ...rest } = newUser._doc;
       res
         .status(200)
