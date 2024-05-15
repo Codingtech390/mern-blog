d241c5cd61e4fbc33f0989b457a364e5c1602ec1
codingtech <codingtech390@gmail.com>
2024-05-15 17:06:31 +0530

    Showed all the comments of a post.

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -1,5 +1,7 @@
 import Comment from "../models/comment.model.js";
 
+// Create a comment
+
 export const createComment = async (req, res, next) => {
   try {
     const { content, postId, userId } = req.body;
@@ -15,6 +17,19 @@ export const createComment = async (req, res, next) => {
     });
     await newComment.save();
     res.status(200).json(newComment);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get comments
+
+export const getPostComments = async (req, res, next) => {
+  try {
+    const comments = await Comment.find({ postId: req.params.postId }).sort({
+      createdAt: -1,
+    });
+    res.status(200).json(comments);
   } catch (error) {
     next(error);
   }
