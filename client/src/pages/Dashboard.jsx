acf3dc23f5bfee4a70d3378243a7734e66d10539
codingtech <codingtech390@gmail.com>
2024-05-15 10:13:24 +0530

    Added Show all the users to the admin dashboard.

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 import DashboardSidebar from "../components/DashboardSidebar";
 import DashboardProfile from "../components/DashboardProfile";
 import DashboardPosts from "../components/DashboardPosts";
+import DashboardUsers from "../components/DashboardUsers";
 export default function Dashboard() {
   const location = useLocation();
   const [tab, setTab] = useState("");
@@ -26,6 +27,8 @@ export default function Dashboard() {
       {tab === 'profile' && <DashboardProfile />}
       {/* Posts */}
       {tab === "posts" && <DashboardPosts />}
+      {/* Users */}
+      {tab === "users" && <DashboardUsers />}
     </div>
   );
 }
