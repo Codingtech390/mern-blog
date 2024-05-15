f8560d096cc95c848662c153bdf46ac8c7542df0
codingtech <codingtech390@gmail.com>
2024-05-15 09:16:17 +0530

    Added update post functionality.

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -79,11 +79,39 @@ export const getposts = async (req, res, next) => {
 
 export const deletePost = async (req, res, next) => {
   if (!req.user.isAdmin || req.user.id !== req.params.userId) {
-    return next(errorHandler(403, "You do not have permission to delete this post"));
+    return next(
+      errorHandler(403, "You do not have permission to delete this post")
+    );
   }
   try {
     await Post.findByIdAndDelete(req.params.postId);
     res.status(200).json({ message: "Post has been deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Update post
+
+export const updatepost = async (req, res, next) => {
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+    return next(
+      errorHandler(403, "You do not have permission to update this post")
+    );
+  }
+
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(req.params.postId, {
+        $set: {
+          title: req.body.title,
+          content: req.body.content,
+          category: req.body.category,
+          image: req.body.image,
+        },
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedPost);
   } catch (error) {
     next(error);
   }
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -4,6 +4,7 @@ import {
   create,
   deletePost,
   getposts,
+  updatepost,
 } from "../controllers/post.controller.js";
 
 const router = express.Router();
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/create", verifyToken, create);
 router.get("/getposts", getposts);
 router.delete("/deletepost/:postId/:userId", verifyToken, deletePost);
+router.put("/updatepost/:postId/:userId", verifyToken, updatepost);
 
 export default router;
